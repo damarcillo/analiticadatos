b8e7e9d3a7b440b48d6559302df03d8cda0a5ef7
damarcillo <88499506+damarcillo@users.noreply.github.com>
2022-09-09 16:02:59 -0500

    Add files via upload

---
+++ veterinaria/agencia.py
@@ -0,0 +1,14 @@
+from typing_extensions import Self
+
+
+class Agencia:
+    
+        Self.idAgencia=( )
+        Self.ciudad=(" ")
+
+def idAgencia(self, idAgencia):
+    print("retorna el id de la agencia de la clinica veterinaria")
+
+def ciudad(self,ciudad):
+    print("retorna la ciudad donde  queda la Agencia de la clinica veterinaria")
+    
---
+++ veterinaria/medicinas.py
@@ -0,0 +1,13 @@
+from typing_extensions import Self
+
+
+class Medicinas:
+    
+        Self.idMedicina=( )
+        Self.valor=(" ")
+
+def idMedicina(self, idMedicina):
+    print("retorna el id del medicina")  
+
+def valor(self,valor):
+    print("retorna el valor del medicina")
