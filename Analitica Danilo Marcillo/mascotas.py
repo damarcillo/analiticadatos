1a3eec4b1ef074400ac6f18a2b948d85d2dcb4b2
damarcillo <88499506+damarcillo@users.noreply.github.com>
2022-09-09 16:00:59 -0500

    Add files via upload

---
+++ Analitica Danilo Marcillo/mascotas.py
@@ -0,0 +1,17 @@
+from typing_extensions import Self
+
+class Mascota:
+    
+        Self.idMascota=( )
+        Self.idDueño=(" ")
+        Self.enfermedad=(" ")
+
+def idMascota(self, idMascota):
+    print("retorna el id de la Mascota")
+    
+def idDueño(self,idDueño):
+    print("retorna el id del Papa de la Mascota")
+    
+def enfermedad(self, enfermedad):
+    print("retorna la enfermedad de la Mascota")
+    
